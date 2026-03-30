e6946a227d2d085fd78964e574bcf0c139b8a186
lbeland <beland@campus.tu-berlin.de>
2026-03-30 15:27:31 +0200

    update

--- extensions/processors/PhaseEstimator/PhaseEstimator.hpp
+++ extensions/processors/PhaseEstimator/PhaseEstimator.hpp
@@ -20,43 +20,52 @@
 
 #include "iprocessor.hpp"
 #include "multichanneldata/multichanneldata.hpp"
+#include <dsp/filter.hpp>
 #include <boost/circular_buffer.hpp>
 #include <complex>
 #include <fftw3.h>
 #include <vector>
 
 class PhaseEstimator : public IProcessor {
- public:
+  public:
     PhaseEstimator();
     int get_max_bin(fftwf_complex* out, size_t out_size);
     void construct_analytic_spectrum(int M, const fftwf_complex* half, fftwf_complex* full);
     void fftshift(const fftwf_complex* in, fftwf_complex* out, int L);
     void ifftshift(const fftwf_complex* in, fftwf_complex* out, int L);
     void ifftshift(const std::vector<std::complex<float>>& in, std::vector<std::complex<float>>& out, int L);
+    void calibrate_gain(const int N);
+    void load_filter_coeffs();
 
+  void Configure(const GlobalContext &context) override;
   void CreatePorts() override;
   void CompleteStreamInfo() override;
   void Preprocess(ProcessingContext &context) override;
   void Process(ProcessingContext &context) override;
   void Postprocess(ProcessingContext &context) override;
 
+  // VARIABLES
+  protected:
+    unsigned int packet_count_ = 0;
+    double first_timestamp_ = 0.0;
+    float fs_ = 0.0;
+    float f0_ = 0.0;
+    inline static boost::circular_buffer<float> sample_window{1};  // Initialized with size 1, will be resized in Preprocess
+    std::string coeff_file_;  // Path to bandpass filter coefficients file
+    std::vector<std::complex<float>> coeffs_;  // Filter coefficients of bandpass filter
+    std::complex<float> c_gain_;  // Calibration gain for cecHT
 
- protected:
-  PortIn<MultiChannelType<float>> *data_in_port_;
-  PortOut<MultiChannelType<float>> *data_out_port_;
+    const uint32_t MAX_NCHANNELS=384;
 
-  options::Value<int, false> n_messages_{-1};
-  options::Value<unsigned int, false> n_fft_{4096};
-  options::Value<bool> calibrate_{false};
-  options::Value<std::string> coeff_file_{"filters://echt_coefficients.txt"};
-  
-  unsigned int packet_count_ = 0;
-  double first_timestamp_ = 0.0;
-  float fs_ = 0.0;
-  float f0_ = 0.0;
-  inline static boost::circular_buffer<float> sample_window{1};  // Initialized with size 1, will be resized in Preprocess
-  std::vector<std::complex<float>> coeffs_;  // Filter coefficients of bandpass filter
-  std::complex<float> c_gain_;  // Calibration gain for cecHT
+  // DATA PORTS
+  protected:
+    PortIn<MultiChannelType<float>> *data_in_port_;
+    PortOut<MultiChannelType<float>> *data_out_port_;
 
-  const uint32_t MAX_NCHANNELS=384;
+  // OPTIONS
+  protected:
+    options::Value<int, false> n_messages_{-1};
+    options::Value<unsigned int, false> n_fft_{4096};
+    options::Value<bool> calibrate_{false};
+    options::Value<YAML::Node, false> filter_def_{};
 };

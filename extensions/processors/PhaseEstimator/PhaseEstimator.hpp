37d2d947e542d4cc897b7072869f44b9bcac594b
lbeland <beland@campus.tu-berlin.de>
2026-04-02 11:12:20 +0200

    update

--- extensions/processors/PhaseEstimator/PhaseEstimator.hpp
+++ extensions/processors/PhaseEstimator/PhaseEstimator.hpp
@@ -67,7 +67,7 @@ class PhaseEstimator : public IProcessor {
     options::Value<unsigned int, false> n_fft_{4096};
     options::Value<bool> calibrate_{false};
     options::Value<float, false> iaf_default_{10.0f};
-    options::Value<unsigned int, false> iaf_read_interval_{500};
+    options::Value<unsigned int, false> iaf_read_interval_{5000};
     options::Value<YAML::Node, false> filter_def_{};
 
     FollowerState<float>* iaf_state_ = nullptr;
--- extensions/processors/Producer/Producer.hpp
+++ extensions/processors/Producer/Producer.hpp
@@ -36,7 +36,7 @@ class Producer : public IProcessor {
   PortOut<MultiChannelType<float>> *data_out_port_;
 
   options::Value<double, false> fs_{10000.0};
-  options::Value<unsigned int, false> f0_{10};
+  options::Value<double, false> f0_{10.0};
   options::Value<unsigned int, false> nchannels_{4};
   options::Value<unsigned int, false> nsamples_{100};
   options::Value<int, false> n_messages_{-1};

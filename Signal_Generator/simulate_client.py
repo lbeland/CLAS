f6d887544cbff984452d5327729a9a1b560bc82b
lbeland <beland@campus.tu-berlin.de>
2026-04-12 18:41:13 +0200

    simulate amplitude and frequency modulation

--- Signal_Generator/simulate_client.py
+++ Signal_Generator/simulate_client.py
@@ -5,13 +5,15 @@ import time
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 25000
+Modulation_type = "angle"   # amplitude, angle or none
+mod_ampl = 3
+mod_freq = 0.5
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Signal parameters
 amplitude = 1.0
-frequency = 8
-phase = 0.0
+frequency = 9
 sample_rate = 100.0
 
 dt = 1.0 / sample_rate
@@ -30,9 +32,23 @@ while True:
     # --- EEG (32 floats) ---
     eeg = []
     for ch in range(32):
-        # Example: same sine on all channels (you can vary per channel)
-        value = amplitude * math.sin(2.0 * math.pi * frequency * t + phase)
+        carrier =  amplitude * math.cos(2.0 * math.pi * frequency * t)
+
+        if Modulation_type != "none":
+            if Modulation_type == "amplitude": 
+                value = carrier + mod_ampl/2 * (
+                                                math.cos(2.0 * math.pi * (frequency + mod_freq) * t) +
+                                                math.cos(2.0 * math.pi * (frequency - mod_freq) * t)
+                                            )
+            elif Modulation_type == "angle":
+                # freq_dev = frequency - mod_freq
+                value = amplitude * math.cos(2.0 * math.pi * frequency * t + (mod_ampl/mod_freq) * math.sin(2.0 * math.pi * mod_freq * t))
+        else:
+            value = carrier
+
         eeg.append(value)
+
+    eeg[9] *= 10    # Make channel 9 stand out for testing
 
     
     # print(f"Time: {t:.3f}s, Sample: {sample_counter}, EEG[0]: {eeg[0]:.3f}")

4af59c10769119405cdb3610932c3240505a15d8
lbeland <beland@campus.tu-berlin.de>
2026-04-20 13:20:45 +0200

    store also current amplitude

--- Signal_Generator/simulate_client.py
+++ Signal_Generator/simulate_client.py
@@ -11,19 +11,21 @@ def ampl_mod(t, A_c, f_c, A_m, f_m):
     w_c = 2.0 * math.pi * f_c
     w_m = 2.0 * math.pi * f_m
     value = (A_c + A_m * math.cos(w_m * t)) * math.cos(w_c * t)
+    amplitude = A_c + A_m * math.cos(w_m * t)
     theta = w_c * t
     inst_freq = w_c / (2.0 * math.pi)
 
-    return value, theta, inst_freq
+    return value, amplitude, theta, inst_freq
 
 def angle_mod(t, A_c, f_c, A_m, f_m):           
     w_c = 2.0 * math.pi * f_c
     w_m = 2.0 * math.pi * f_m
     value = A_c * math.cos(w_c * t + (A_m/f_m) * math.sin(w_m * t))
+    amplitude = A_c
     theta = w_c * t + (A_m/f_m) * math.sin(w_m * t)
     inst_freq = f_c + A_m * math.cos(w_m * t)
 
-    return value, theta, inst_freq
+    return value, amplitude, theta, inst_freq
 
 def client_simulate(n_samples=-1):
     UDP_IP = "127.0.0.1"
@@ -40,7 +42,7 @@ def client_simulate(n_samples=-1):
     t = 0.0
 
     # Modulation parameters
-    Modulation_type = "amplitude"   # amplitude, phase or None
+    Modulation_type = "phase"   # amplitude, phase or None
     mod_signal = {'amplitude': 1.0, 'frequency': 0.1}
 
     print(f"Sending structured packets to {UDP_IP}:{UDP_PORT}")
@@ -69,20 +71,21 @@ def client_simulate(n_samples=-1):
 
             if Modulation_type != "None":
                 if Modulation_type == "amplitude": 
-                    value, theta, inst_freq = ampl_mod(t, carrier_signal["amplitude"], carrier_signal["frequency"], mod_signal["amplitude"], mod_signal["frequency"])
+                    value, amplitude, theta, inst_freq = ampl_mod(t, carrier_signal["amplitude"], carrier_signal["frequency"], mod_signal["amplitude"], mod_signal["frequency"])
 
                 elif Modulation_type == "phase":
-                    value, theta, inst_freq = angle_mod(t, carrier_signal["amplitude"], carrier_signal["frequency"], mod_signal["amplitude"], mod_signal["frequency"])
+                    value, amplitude, theta, inst_freq = angle_mod(t, carrier_signal["amplitude"], carrier_signal["frequency"], mod_signal["amplitude"], mod_signal["frequency"])
 
             else:
                 value = carrier_signal["amplitude"] * math.cos(2.0 * math.pi * carrier_signal["frequency"] * t)
+                amplitude = carrier_signal["amplitude"]
                 theta = 2.0 * math.pi * carrier_signal["frequency"] * t
                 inst_freq = carrier_signal["frequency"]
 
             for ch in range(32):
                 eeg.append(value)
 
-            storage.append((t, value, theta, inst_freq))
+            storage.append((t, value, amplitude, theta, inst_freq))
 
             # eeg[9] *= 10    # Make channel 9 stand out for testing
 
@@ -121,7 +124,7 @@ def client_simulate(n_samples=-1):
     except KeyboardInterrupt:
         print("Simulation interrupted by user")
     finally:
-        np.save("simulated_signal.npy", np.array(storage, dtype=[("time", "f4"), ("value", "f4"), ("phase", "f4"), ("inst_freq", "f4")]))
+        np.save("simulated_signal.npy", np.array(storage, dtype=[("time", "f4"), ("value", "f4"), ("amplitude", "f4"), ("phase", "f4"), ("inst_freq", "f4")]))
 
 
 def main():

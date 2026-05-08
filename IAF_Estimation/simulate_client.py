fe09a808cfce29fe694391782dceb88b255c05da
lbeland <beland@campus.tu-berlin.de>
2026-05-08 21:56:03 +0200

    only use angle modulation if modulation frequency greater zero

--- IAF_Estimation/simulate_client.py
+++ IAF_Estimation/simulate_client.py
@@ -20,10 +20,16 @@ def ampl_mod(t, A_c, f_c, A_m, f_m):
 def angle_mod(t, A_c, f_c, A_m, f_m):           
     w_c = 2.0 * math.pi * f_c
     w_m = 2.0 * math.pi * f_m
-    value = A_c * math.cos(w_c * t + (A_m/f_m) * math.sin(w_m * t))
-    amplitude = A_c
-    theta = w_c * t + (A_m/f_m) * math.sin(w_m * t)
-    inst_freq = f_c + A_m * math.cos(w_m * t)
+    if w_m > 0:
+        value = A_c * math.cos(w_c * t + (A_m/f_m) * math.sin(w_m * t))
+        amplitude = A_c
+        theta = w_c * t + (A_m/f_m) * math.sin(w_m * t)
+        inst_freq = f_c + A_m * math.cos(w_m * t)
+    else:
+        value = A_c * math.cos(w_c * t)
+        amplitude = A_c
+        theta = w_c * t
+        inst_freq = f_c
 
     return value, amplitude, theta, inst_freq
 
